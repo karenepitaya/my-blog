12cfd27cf03b6093351542babe17f20ce389ce88
Karene Pitaya <lin425173@gmail.com>
2025-12-21 04:08:02 +0800

    feat(public-api): add public category and tag endpoints

--- server/src/routes/publicRoutes.ts
+++ server/src/routes/publicRoutes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import publicArticleRoutes from './publicArticleRoutes';
+import publicCategoryRoutes from './publicCategoryRoutes';
+import publicTagRoutes from './publicTagRoutes';
 
 const router: Router = Router();
 
 router.get('/', (req, res) => res.success({ route: 'publicRoutes' }));
 
 router.use('/public/articles', publicArticleRoutes);
+router.use('/public/categories', publicCategoryRoutes);
+router.use('/public/tags', publicTagRoutes);
 
 export default router;
