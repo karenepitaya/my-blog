0c6706972fb630bd51b5729575a672c2ee7eb6bb
Karene Pitaya <lin425173@gmail.com>
2025-12-21 04:26:19 +0800

    chore(repo): remove legacy frontend and admin panel

--- frontend/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  // reactCompiler: true, // 暂时禁用React Compiler以解决RSC错误
-};
-
-export default nextConfig;
