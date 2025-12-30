3f2cb6d181a1909715c1fce335cbd4791a9bbf70
Karene Pitaya <lin425173@gmail.com>
2025-12-30 16:25:41 +0800

    feat(frontend): load expressive-code assets for posts

--- frontend/src/site.config.ts
+++ frontend/src/site.config.ts
@@ -1,66 +1,45 @@
 import type { SiteConfig } from '~/types'
 
 const config: SiteConfig = {
-  // Absolute URL to the root of your published site, used for generating links and sitemaps.
   site: 'https://blog.karenepitaya.xyz',
-  // The name of your site, used in the title and for SEO.
   title: 'Karene\'s Blog',
-  // The description of your site, used for SEO and RSS feed.
-  description:
-    'A multi-author blog powered by our server public API',
-  // The author of the site, used in the footer, SEO, and RSS feed.
+  description: 'A multi-author blog powered by our server public API',
   author: 'Karene Pitayas',
-  // Keywords for SEO, used in the meta tags.
-  tags: ['Astro', 'MultiTerm', 'Blog', 'karenepitaya'],
-  // Path to the image used for generating social media previews.
-  // Needs to be a square JPEG file due to limitations of the social card generator.
-  // Try https://squoosh.app/ to easily convert images to JPEG.
+  tags: [
+    'Astro',
+    'MultiTerm',
+    'Blog',
+    'karenepitaya'
+  ],
   socialCardAvatarImage: './src/content/avatar.jpg',
-  // Font imported from @fontsource or elsewhere, used for the entire site.
-  // To change this see src/styles/global.css and import a different font.
   font: 'JetBrains Mono Variable',
-  // For pagination, the number of posts to display per page.
-  // The homepage will display half this number in the "Latest Posts" section.
   pageSize: 6,
-  // Whether Astro should resolve trailing slashes in URLs or not.
-  // This value is used in the astro.config.mjs file and in the "Search" component to make sure pagefind links match this setting.
-  // It is not recommended to change this, since most links existing in the site currently do not have trailing slashes.
   trailingSlashes: false,
-  // The navigation links to display in the header.
   navLinks: [
     {
       name: 'Home',
       url: '/',
+      external: undefined
     },
     {
       name: 'About',
       url: '/about',
+      external: undefined
     },
     {
       name: 'Archive',
       url: '/posts',
+      external: undefined
     },
     {
       name: 'GitHub',
       url: 'https://github.com/karenepitaya',
-      external: true,
-    },
+      external: true
+    }
   ],
-  // The theming configuration for the site.
   themes: {
-    // The theming mode. One of "single" | "select" | "light-dark-auto".
     mode: 'select',
-    // The default theme identifier, used when themeMode is "select" or "light-dark-auto".
-    // Make sure this is one of the themes listed in `themes` or "auto" for "light-dark-auto" mode.
     default: 'catppuccin-mocha',
-    // Shiki themes to bundle with the site.
-    // https://expressive-code.com/guides/themes/#using-bundled-themes
-    // These will be used to theme the entire site along with syntax highlighting.
-    // To use light-dark-auto mode, only include a light and a dark theme in that order.
-    // include: [
-    //   'github-light',
-    //   'github-dark',
-    // ]
     include: [
       'andromeeda',
       'aurora-x',
@@ -121,59 +100,24 @@ const config: SiteConfig = {
       'vesper',
       'vitesse-black',
       'vitesse-dark',
-      'vitesse-light',
+      'vitesse-light'
     ],
-    // Optional overrides for specific themes to customize colors.
-    // Their values can be either a literal color (hex, rgb, hsl) or another theme key.
-    // See themeKeys list in src/types.ts for available keys to override and reference.
-    overrides: {
-      // Improve readability for aurora-x theme
-      // 'aurora-x': {
-      //   background: '#292929FF',
-      //   foreground: '#DDDDDDFF',
-      //   warning: '#FF7876FF',
-      //   important: '#FF98FFFF',
-      //   note: '#83AEFFFF',
-      // },
-      // Make the GitHub dark theme a little cuter
-      // 'github-light': {
-      //   accent: 'magenta',
-      //   heading1: 'magenta',
-      //   heading2: 'magenta',
-      //   heading3: 'magenta',
-      //   heading4: 'magenta',
-      //   heading5: 'magenta',
-      //   heading6: 'magenta',
-      //   separator: 'magenta',
-      //   link: 'list',
-      // },
-    },
+    overrides: undefined
   },
-  // Social links to display in the footer.
   socialLinks: {
     github: 'https://github.com/karenepitaya',
-    mastodon: 'https://github.com/karenepitaya',
-    email: 'https://github.com/karenepitaya',
-    linkedin: 'https://github.com/karenepitaya',
-    bluesky: 'https://github.com/karenepitaya',
     twitter: 'https://github.com/karenepitaya',
+    mastodon: 'https://github.com/karenepitaya',
+    bluesky: 'https://github.com/karenepitaya',
+    linkedin: 'https://github.com/karenepitaya',
+    email: 'https://github.com/karenepitaya'
   },
-  // Configuration for Giscus comments.
-  // To set up Giscus, follow the instructions at https://giscus.app/
-  // You'll need a GitHub repository with discussions enabled and the Giscus app installed.
-  // Take the values from the generated script tag at https://giscus.app and fill them in here.
-  // IMPORTANT: Update giscus.json in the root of the project with your own website URL
-  // If you don't want to use Giscus, set this to undefined.
-  giscus: undefined,
-  // These are characters available for the character chat feature.
-  // To add your own character, add an image file to the top-level `/public` directory
-  // Make sure to compress the image to a web-friendly size (<100kb)
-  // Try using the excellent https://squoosh.app web app for creating small webp files
   characters: {
     owl: '/owl.webp',
     unicorn: '/unicorn.webp',
-    duck: '/duck.webp',
+    duck: '/duck.webp'
   },
+  giscus: undefined
 }
 
 export default config
